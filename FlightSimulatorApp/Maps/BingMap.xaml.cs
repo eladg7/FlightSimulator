c4530695935b44c40f780cc2b90043d3782444b9
elad g <eladgarber@gmail.com>
2020-03-27 16:39:23 +0300

    reading from app config

--- FlightSimulatorApp/Maps/BingMap.xaml.cs
+++ FlightSimulatorApp/Maps/BingMap.xaml.cs
@@ -1,24 +1,7 @@
-﻿using FlightSimulatorApp.Maps;
+﻿using System.Windows.Controls;
 using Microsoft.Maps.MapControl.WPF;
-using Microsoft.Maps.MapControl.WPF.Design;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace FlightSimulatorApp
+namespace FlightSimulatorApp.Maps
 {
     /// <summary>
     /// Interaction logic for BingMap.xaml
